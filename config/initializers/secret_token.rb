5be98d0c2c2a2ff65ac7b18659e1e7a38516008a
Max Rogers <maxrogers90@gmail.com>
2013-12-18 19:02:45 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dads::Application.config.secret_key_base = '39229baa3c2141d697fdae7f14dfe623999cc9f9e2bbdec71d19b8313ab38706040ad942d25ce9c8c5a30df3cea55ce0b0d0cc645955c9c75a300588b08edd6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dads::Application.config.session_store :cookie_store, key: '_dads_session'
